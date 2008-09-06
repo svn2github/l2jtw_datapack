79c232faa4ed8d92a01cd6c6f980e531d65d66b8
rocknow <rocknow@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2008-09-06 16:34:12 +0000

    上傳CT2-SQL
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@21 9bd44e3e-6552-0410-907e-a6ca40b856b6

---
+++ L2JTW_Datapack_CT2/sql/npcCharData.sql
@@ -0,0 +1,27 @@
+-- ----------------------------
+-- Table structure for npcchardata
+-- ----------------------------
+DROP TABLE IF EXISTS npcchardata;
+CREATE TABLE `npcchardata` (
+  `npc_id` int(11) NOT NULL DEFAULT '0',
+  `ischar` int(11) DEFAULT NULL,
+  `charclass` int(11) DEFAULT NULL,
+  `charrace` int(11) DEFAULT NULL,
+  `charface` int(11) DEFAULT NULL,
+  `charhair` int(11) DEFAULT NULL,
+  `charcolor` int(11) DEFAULT NULL,
+  `charhero` int(11) DEFAULT NULL,
+  `charsex` int(11) DEFAULT NULL,
+  `lrhand` decimal(11,0) DEFAULT NULL,
+  `enchlvl` decimal(11,0) DEFAULT NULL,
+  `armor` decimal(11,0) DEFAULT NULL,
+  `pant` decimal(11,0) DEFAULT NULL,
+  `head` decimal(11,0) DEFAULT NULL,
+  `boot` decimal(11,0) DEFAULT NULL,
+  `glove` decimal(11,0) DEFAULT NULL,
+  `hair` decimal(11,0) DEFAULT NULL,
+  `dhair` decimal(11,0) DEFAULT NULL,
+  `face` decimal(11,0) DEFAULT NULL,
+  `augmentation` decimal(11,0) DEFAULT NULL,
+  PRIMARY KEY (`npc_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
