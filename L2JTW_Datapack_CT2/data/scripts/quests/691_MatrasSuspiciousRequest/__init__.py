09039ae3070c9479f9ce1317902334506262776f
rocknow <rocknow@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2009-02-21 19:46:35 +0000

    上傳pmq(飛天豬)收集的官方對話(任務183.660.691)/(sync 5721)
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@199 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_CT2/data/scripts/quests/691_MatrasSuspiciousRequest/__init__.py
+++ L2JTW_Datapack_CT2/data/scripts/quests/691_MatrasSuspiciousRequest/__init__.py
@@ -1,3 +1,4 @@
+#pmq
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
@@ -5,27 +6,47 @@ from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
 qn = "691_MatrasSuspiciousRequest"
 
-# NPC
-32245
+# Npc
+MATRAS = 32245
 
-MOBS = [22398, 22399]
+MOBS = [22363,22364,22365,22366,22367,22368,22369,22370,22371,22372]
 
-# Items
-BALL = 10372
+# Quest Item
+REDSTONE = 10372             # ¬õ¥Û
+
+ADENA = 57                   # ª÷¹ô
+DYNASTICESSENCEII = 10413    # ¤ý´Â¤§»îII
 
 class Quest (JQuest) :
 
   def __init__(self, id, name, descr) : 
     JQuest.__init__(self, id, name, descr)
+    self.questItemIds = [REDSTONE]
 
   def onEvent (self, event, st) :
     htmltext = event
-    if event == "32245-02.htm" :
+    RED = st.getQuestItemsCount(REDSTONE)
+    if event == "32245-03.htm" :
       if st.getPlayer().getLevel() >= 76 :
         st.set("cond","1")
         st.setState(State.STARTED)
         st.playSound("ItemSound.quest_accept")
+      else :
+        htmltext = "32245-00.htm"
         st.exitQuest(1)
+    elif event == "6" :
+      if RED >= 744 :
+        htmltext = "32245-06.htm"
+        st.takeItems(REDSTONE,-744)
+        st.giveItems(DYNASTICESSENCEII,1)
+        st.playSound("ItemSound.quest_giveup")
+      else :
+        htmltext = "32245-09.htm"
+    elif event == "32245-08.htm" :
+        st.takeItems(RED,-1)
+        st.giveItems(ADENA,RED*10000)
+        st.exitQuest(1)
+        st.playSound("ItemSound.quest_finish")
     return htmltext    
 
   def onTalk (self, npc, player) :
@@ -34,16 +55,18 @@ class Quest (JQuest) :
      if st :
        id = st.getState()
        cond = st.getInt("cond")
+       RED = st.getQuestItemsCount(REDSTONE)
        if id == State.CREATED :
          if player.getLevel() >= 76 :
-           htmltext="32245-00.htm"
-           st.exitQuest(1)
+           htmltext="32245-01.htm"
          else :
            htmltext="32245-00.htm"
            st.exitQuest(1)
        else :
-         if cond == 1 :
-           htmltext = "32245-03.htm"
+         if cond == 1 and RED >= 1 :
+           htmltext = "32245-05.htm"
+         else :
+           htmltext = "32245-04.htm"
      return htmltext
 
   def onKill(self, npc, player, isPet) :
@@ -52,6 +75,8 @@ class Quest (JQuest) :
     st = partyMember.getQuestState(qn)
     if st :
       if st.getState() == State.STARTED :
+        if st.getRandom(100) < 80 :
+          st.giveItems(REDSTONE, 1)
           st.playSound("ItemSound.quest_itemget")
     return
 
@@ -61,5 +86,5 @@ QUEST.addStartNpc(32245)
 
 QUEST.addTalkId(32245)
 
-for mobId in MOBS :
+for mobId in MOBS:
   QUEST.addKillId(mobId)
