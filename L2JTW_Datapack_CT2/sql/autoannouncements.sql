ebbf10509568be16fc56690f943f61071f4232f1
shansoft <shansoft@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2009-03-19 09:09:05 +0000

    自動循環式公告資料, 真正資料請等原始碼更新.
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@213 9bd44e3e-6552-0410-907e-a6ca40b856b6

---
+++ L2JTW_Datapack_CT2/sql/autoannouncements.sql
@@ -0,0 +1,16 @@
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for autoannouncements
+-- ----------------------------
+CREATE TABLE `autoannouncements` (
+  `id` int(11) NOT NULL,
+  `initial` bigint(20) NOT NULL,
+  `delay` bigint(20) NOT NULL,
+  `memo` text,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records 
+-- ----------------------------
