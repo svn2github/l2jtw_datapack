948d229342583757e5cb9701c6fb6225e0b704e8
rocknow <rocknow@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2009-04-28 20:44:19 +0000

    更新破滅國境官方(感謝 pmq 提供)
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@304 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_CT2/data/scripts/custom/2510_Flyboatlicense/__init__.py
+++ L2JTW_Datapack_CT2/data/scripts/custom/2510_Flyboatlicense/__init__.py
@@ -7,20 +7,26 @@ from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
 qn = "2510_Flyboatlicense"
 
+# ITEM
+POWERSTARSTONE = 13277
+LICENSE = 13559
+
 class Quest (JQuest) :
 
  def __init__(self,id,name,descr) : JQuest.__init__(self,id,name,descr)
 
  def onAdvEvent (self,event,npc,player) :
+  st = player.getQuestState(qn)
+  if not st: return
   htmltext = event
   if event == "32557-0.htm" :
-    if player.isClanLeader() and player.getClan() :
-      if clan.getLevel() <= 5 :
-         st.exitQuest(1)
+    if player.isClanLeader() :
+      if player.getClan().getLevel() < 5 :
+          st.exitQuest(1)
       else :
-         if st.getQuestItemsCount(13277) >= 10 :
-            st.takeItems(13277,10)
-            st.giveItems(13559,1)
+         if st.getQuestItemsCount(POWERSTARSTONE) >= 10 :
+            st.takeItems(POWERSTARSTONE,10)
+            st.giveItems(LICENSE,1)
             htmltext = ""
             st.exitQuest(1)
          else :
@@ -29,13 +35,13 @@ class Quest (JQuest) :
   return htmltext
 
  def onTalk (self,npc,player) :
-  htmltext = "<html><body>目前沒有執行任務，或條件不符。</body></html>"
+  htmltext = ""
   st = player.getQuestState(qn)
   if not st : return htmltext
   npcId = npc.getNpcId()
   id = st.getState()
   if npcId == 32557 :
-    if st.getQuestItemsCount(13559) == 0 :
+    if st.getQuestItemsCount(LICENSE) == 0 :
        htmltext =  "32557-1.htm"
     else :
        st.exitQuest(1)
