799a769a54bcffdc0565eb744342a999a9134ec3
rocknow <rocknow@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2009-06-06 20:43:35 +0000

    貴族通行證改奧林匹亞標章(感謝 pmq 提供)
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@388 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_CT2/data/scripts/teleports/2000_NoblesseTeleport/__init__.py
+++ L2JTW_Datapack_CT2/data/scripts/teleports/2000_NoblesseTeleport/__init__.py
@@ -7,8 +7,8 @@ from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 qn = "2000_NoblesseTeleport"
 NPC=[30006,30059,30080,30134,30146,30177,30233,30256,30320,30540,30576,30836,30848,30878,30899,31275,31320,31964,32163]
 
-html = '<html><body>啊，您是貴族呀，對貴族有提供特別的服務。<br><br>使用貴族通行證<br>\
-        <a action="bypass -h %bypass%">傳送到狩獵場</a><br><br>不使用貴族通行證<br>\
+html = '<html><body>啊，您是貴族呀，對貴族有提供特別的服務。<br><br>使用奧林匹亞標章<br>\
+        <a action="bypass -h %bypass%">傳送到狩獵場</a><br><br>不使用奧林匹亞標章<br>\
         <a action="bypass -h npc_%objectId%_Chat 2">傳送到狩獵場</a><br><a action="bypass -h npc_%objectId%_Chat 0">返回</a></body></html>'
 
 class Quest (JQuest) :
@@ -22,7 +22,7 @@ class Quest (JQuest) :
     st = player.getQuestState(qn)
     if player.isNoble() == 1 :
       bypass = 'Quest 2000_NoblesseTeleport noble-nopass.htm'
-      if st.getQuestItemsCount(6651):
+      if st.getQuestItemsCount(13722):
          bypass = 'npc_%objectId%_Chat 3'
       htmltext=html.replace("%bypass%",str(bypass)).replace("%objectId%",str(npc.getObjectId()))
     else :
