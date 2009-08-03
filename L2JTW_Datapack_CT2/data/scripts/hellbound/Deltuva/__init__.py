a5bb179679dd88c295a681e9781deebaecb32478
rocknow <rocknow@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2009-08-03 20:28:32 +0000

    更新/修正
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@470 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_CT2/data/scripts/hellbound/Deltuva/__init__.py
+++ L2JTW_Datapack_CT2/data/scripts/hellbound/Deltuva/__init__.py
@@ -23,7 +23,7 @@ class Quest (JQuest) :
 		htmltext = "<html><body>道爾土巴：<br>看來你們還沒有進去過鋼鐵之城喔。因為太過危險，所以無法讓你們進去。</body></html>"
 		if st1 :
 			if npcId == DELTUVA and st1.getState() == State.COMPLETED :
-				player.teleToLocation(-12627, 273815, -15305)
+				player.teleToLocation(17957, 283361, -9705)
 				htmltext = "<html><body>道爾土巴：<br><font color=\"FF0000\">（這是測試傳送尚未實裝！）</font></body></html>"
 				st.exitQuest(1)
 		st.exitQuest(1)
