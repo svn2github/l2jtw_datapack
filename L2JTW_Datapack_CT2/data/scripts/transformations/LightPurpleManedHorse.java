5873c1ae2e1868d06d6113b1e0d306831696e6ca
rocknow <rocknow@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2009-08-26 22:55:44 +0000

    sync 6513
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@487 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_CT2/data/scripts/transformations/LightPurpleManedHorse.java
+++ L2JTW_Datapack_CT2/data/scripts/transformations/LightPurpleManedHorse.java
@@ -4,13 +4,12 @@ import net.sf.l2j.gameserver.datatables.SkillTable;
 import net.sf.l2j.gameserver.instancemanager.TransformationManager;
 import net.sf.l2j.gameserver.model.L2Transformation;
 
-
 public class LightPurpleManedHorse extends L2Transformation
 {
 	public LightPurpleManedHorse()
 	{
 		// id, colRadius, colHeight
-		super(106, 12.0, 32.0);
+		super(106, 12, 32);
 	}
 
 	public void onTransform()
@@ -18,33 +17,31 @@ public class LightPurpleManedHorse extends L2Transformation
 		if (getPlayer().getTransformationId() != 106 || getPlayer().isCursedWeaponEquipped())
 			return;
 
-		// give transformation skills
 		transformedSkills();
 	}
 
 	public void transformedSkills()
 	{
-		// Dismount
-		getPlayer().addSkill(SkillTable.getInstance().getInfo(839, 1), false);
 		// Decrease Bow/Crossbow Attack Speed
 		getPlayer().addSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+		// Dismount
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(839, 1), false);
 		getPlayer().addSkill(SkillTable.getInstance().getInfo(5437, 1), false);//Update by rocknow
 
-		getPlayer().setTransformAllowedSkills(new int[]{839,5491,5437});//Update by rocknow
+		getPlayer().setTransformAllowedSkills(new int[]{5491,839,5437});//Update by rocknow
 	}
 
 	public void onUntransform()
 	{
-		// remove transformation skills
 		removeSkills();
 	}
 
 	public void removeSkills()
 	{
-		// Dismount
-		getPlayer().removeSkill(SkillTable.getInstance().getInfo(839, 1), false);
 		// Decrease Bow/Crossbow Attack Speed
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+		// Dismount
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(839, 1), false);
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(5437, 1), false);//Update by rocknow
 
 		getPlayer().setTransformAllowedSkills(new int[]{});
