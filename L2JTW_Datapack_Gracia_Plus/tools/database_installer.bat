6847103a81a2f85a27b2b984edcd393570bcc953
shansoft <shansoft@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2010-03-31 03:28:42 +0000

    sql 更新
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@713 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_Gracia_Plus/tools/database_installer.bat
+++ L2JTW_Datapack_Gracia_Plus/tools/database_installer.bat
@@ -781,6 +781,9 @@ spawnlist.sql
 special_skill_trees.sql
 teleport.sql
 topic.sql
+territories.sql
+territory_registrations.sql
+territory_spawnlist.sql
 transform_skill_trees.sql
 walker_routes.sql
 weapon.sql
