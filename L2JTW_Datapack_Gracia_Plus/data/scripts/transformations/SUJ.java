6fc66eb22387b7d35d63ecc241c820a73fb219c5
rocknow <rocknow@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2010-06-29 20:29:06 +0000

    GM選單增加變身(特雷坎/特雷卓/蘇琴)
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@771 9bd44e3e-6552-0410-907e-a6ca40b856b6

---
+++ L2JTW_Datapack_Gracia_Plus/data/scripts/transformations/SUJ.java
@@ -0,0 +1,53 @@
+package transformations;
+
+import com.l2jserver.gameserver.datatables.SkillTable;
+import com.l2jserver.gameserver.instancemanager.TransformationManager;
+import com.l2jserver.gameserver.model.L2Transformation;
+
+public class SUJ extends L2Transformation
+{
+	private static final int[] SKILLS = {5491,619};
+	public SUJ()
+	{
+		// id, colRadius, colHeight
+		super(20003, 11, 11);
+	}
+
+	public void onTransform()
+	{
+		if (getPlayer().getTransformationId() != 20003 || getPlayer().isCursedWeaponEquipped())
+			return;
+
+		transformedSkills();
+	}
+
+	public void transformedSkills()
+	{
+		// Decrease Bow/Crossbow Attack Speed
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+		// Dismount
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(619, 1), false);
+
+		getPlayer().setTransformAllowedSkills(SKILLS);
+	}
+
+	public void onUntransform()
+	{
+		removeSkills();
+	}
+
+	public void removeSkills()
+	{
+		// Decrease Bow/Crossbow Attack Speed
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+		// Dismount
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(619, 1), false);
+
+		getPlayer().setTransformAllowedSkills(EMPTY_ARRAY);
+	}
+
+	public static void main(String[] args)
+	{
+		TransformationManager.getInstance().registerTransformation(new SUJ());
+	}
+}
