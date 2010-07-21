602c85cbfd26327fc81f9f740716e9413c256cc2
pmq <pmq@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2010-07-21 06:50:22 +0000

    = =
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@817 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_Gracia_Plus/L2JTW_Addon/SQL_TW/custom.sql
+++ L2JTW_Datapack_Gracia_Plus/L2JTW_Addon/SQL_TW/custom.sql
@@ -13,20 +13,4 @@ UPDATE `castle` SET `name` = '高達特城' WHERE `id` = 7;
 UPDATE `castle` SET `name` = '魯因城' WHERE `id` = 8;
 UPDATE `castle` SET `name` = '修加特城' WHERE `id` = 9;
 
-/***** merchant_areas_list.sql *****/
-UPDATE `merchant_areas_list` SET `merchant_area_name` = '新手村莊' WHERE `merchant_area_id` = 1;
-UPDATE `merchant_areas_list` SET `merchant_area_name` = '黑暗精靈森林' WHERE `merchant_area_id` = 2;
-UPDATE `merchant_areas_list` SET `merchant_area_name` = '古魯丁村莊' WHERE `merchant_area_id` = 3;
-UPDATE `merchant_areas_list` SET `merchant_area_name` = '古魯丁城鎮' WHERE `merchant_area_id` = 4;
-UPDATE `merchant_areas_list` SET `merchant_area_name` = '荒原南方' WHERE `merchant_area_id` = 5;
-UPDATE `merchant_areas_list` SET `merchant_area_name` = '狄恩城鎮' WHERE `merchant_area_id` = 6;
-UPDATE `merchant_areas_list` SET `merchant_area_name` = '芙羅蘭村莊' WHERE `merchant_area_id` = 7;
-UPDATE `merchant_areas_list` SET `merchant_area_name` = '獵人村莊' WHERE `merchant_area_id` = 8;
-UPDATE `merchant_areas_list` SET `merchant_area_name` = '奇岩城鎮' WHERE `merchant_area_id` = 9;
-UPDATE `merchant_areas_list` SET `merchant_area_name` = '歐瑞城鎮' WHERE `merchant_area_id` = 10;
-UPDATE `merchant_areas_list` SET `merchant_area_name` = '象牙塔' WHERE `merchant_area_id` = 11;
-UPDATE `merchant_areas_list` SET `merchant_area_name` = '哈汀學院' WHERE `merchant_area_id` = 12;
-UPDATE `merchant_areas_list` SET `merchant_area_name` = '亞丁城鎮' WHERE `merchant_area_id` = 13;
-UPDATE `merchant_areas_list` SET `merchant_area_name` = '城堡' WHERE `merchant_area_id` = 14;
-
 /***** Done by vdmyagami！ *****/
