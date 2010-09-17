14e995f0a9f3f3d33a6575b4a2b5947dbe2b7e21
pmq <pmq@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2010-09-17 08:46:05 +0000

    更新642任務
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@924 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_Gracia_Plus/data/scripts/quests/642_APowerfulPrimevalCreature/__init__.py
+++ L2JTW_Datapack_Gracia_Plus/data/scripts/quests/642_APowerfulPrimevalCreature/__init__.py
@@ -1,4 +1,5 @@
 # Created by Gigiikun
+# Update by pmq 17-09-2010
 import sys
 from com.l2jserver import Config
 from com.l2jserver.gameserver.model.quest import State
@@ -16,8 +17,20 @@ ALT_RP_100=0
 
 DINOSAUR_TISSUE = 8774
 DINOSAUR_EGG = 8775
-DINOSAURS = [22196,22197,22198,22199,22200,22201,22202,22203,22204,22205,22218,22219,22220,22223,22224,22225,18344]
+#DINOSAURS = [22196,22197,22198,22199,22200,22201,22202,22203,22204,22205,22218,22219,22220,22223,22224,22225,18344] OLD MOB
+DINOSAURS = [22199,22215,22216,22217,22196,22197,22198,22218,18344]
 REWARDS = [8690,8692,8694,8696,8698,8700,8702,8704,8706,8708,8710]
+REWARDS_W = {
+    "1" : [9967 , 1], # 製作卷軸(王朝劍60%)
+    "2" : [9968 , 1], # 製作卷軸(王朝之刃60%)
+    "3" : [9969 , 1], # 製作卷軸(王朝幻影劍60%)
+    "4" : [9970 , 1], # 製作卷軸(王朝弓60%)
+    "5" : [9971 , 1], # 製作卷軸(王朝小刀60%)
+    "6" : [9972 , 1], # 製作卷軸(王朝長柄戰戟60%)
+    "7" : [9973 , 1], # 製作卷軸(王朝戰鎚60%)
+    "8" : [9974 , 1], # 製作卷軸(王朝釘鎚60%)
+    "9" : [9975 , 1], # 製作卷軸(王朝拳套爪60%)
+    }
 
 class Quest (JQuest) :
 
@@ -36,12 +49,12 @@ class Quest (JQuest) :
 			st.set("cond","1")
 			st.setState(State.STARTED)
 			st.playSound("ItemSound.quest_accept")
-		elif event == "32105-06a.htm" :   # pmq 修改
+		elif event == "32105-06a.htm" :
 			st.takeItems(DINOSAUR_TISSUE,-1)
 			st.giveItems(57,count_tissue*5000)
 		elif event == "32105-07.htm" :
 			if count_tissue < 150 or count_egg == 0 :
-				htmltext = "32105-07a.htm"  # pmq 修改
+				htmltext = "32105-07a.htm"
 			elif ALT_RP_100 != 0 :
 				htmltext = st.showHtmlFile("32105-07.htm").replace("60%","100%")
 		elif event.isdigit() and int(event) in REWARDS :
@@ -55,7 +68,21 @@ class Quest (JQuest) :
 				else :
 					st.giveItems(int(event),1)
 			else :
-				htmltext = "32105-07a.htm"  # pmq 修改
+				htmltext = "32105-07a.htm"
+		elif event == "32105-10.htm" :
+			if count_tissue >= 450 :
+				htmltext = "32105-10.htm"
+			else :
+				htmltext = "32105-11.htm"
+		elif event in REWARDS_W.keys() :
+			if count_tissue >= 450 :
+				item, amount = REWARDS_W[event]
+				st.takeItems(DINOSAUR_TISSUE,450)
+				st.rewardItems(item, amount)
+				st.playSound("ItemSound.quest_itemget")
+				htmltext = "32105-10.htm"
+			else :
+				htmltext = "32105-11.htm"
 		return htmltext
 
 	def onTalk (self,npc,player):
@@ -104,11 +131,9 @@ class Quest (JQuest) :
 					if st.getRandom(100) < chance : 
 						numItems += 1
 					if numItems :
-						if int(count + numItems)/150 > int(count)/150 and itemId == DINOSAUR_TISSUE :
-							st.playSound("ItemSound.quest_middle")
-						else :
+						if int(count + numItems) and itemId == DINOSAUR_TISSUE :
 							st.playSound("ItemSound.quest_itemget")
-						st.giveItems(itemId,int(numItems))
+							st.giveItems(itemId,int(numItems))
 		return
 
 QUEST		= Quest(642,qn,"有關太古強力生物的研究")
