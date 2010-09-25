33740478d007776d81b4e2d6418896184054ce1f
pmq <pmq@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2010-09-25 00:38:32 +0000

    更新 179 182 任務中文化
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@937 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_Gracia_Plus/data/scripts/quests/179_IntoTheLargeCavern/__init__.py
+++ L2JTW_Datapack_Gracia_Plus/data/scripts/quests/179_IntoTheLargeCavern/__init__.py
@@ -1,20 +1,31 @@
 # Update by pmq 14-09-2010
 
 import sys
-from com.l2jserver.gameserver.model.quest import State
-from com.l2jserver.gameserver.model.quest import QuestState
-from com.l2jserver.gameserver.model.quest.jython import QuestJython as JQuest
+from com.l2jserver.gameserver.instancemanager      import InstanceManager
+from com.l2jserver.gameserver.model.actor.instance import L2PcInstance
+from com.l2jserver.gameserver.model.entity         import Instance
+from com.l2jserver.gameserver.model.quest          import State
+from com.l2jserver.gameserver.model.quest          import QuestState
+from com.l2jserver.gameserver.model.quest.jython   import QuestJython as JQuest
+from com.l2jserver.util                            import Rnd
 
 qn = "179_IntoTheLargeCavern"
 
 #NPC'S
-KEKROPUS = 32138
+KEKROPUS             = 32138 # 長老 凱克洛普斯
+MENACING_MACHINE     = 32258 # 巨大機械裝置
+#ITEM
+PUMA_SKIN_SHIRT      = 10021 # 水晶獅皮襯衫
+PUMA_SKIN_GAITERS    = 10022 # 水晶獅皮脛甲
+RED_CRESCENT_EARRING = 10122 # 紅新月耳環
+MECKLACE_OF_DEVOTION = 10123 # 奉獻項鍊
+RING_OF_DEVOTION     = 10124 # 奉獻戒指
 
 class Quest (JQuest) :
 
 	def __init__(self,id,name,descr):
 		JQuest.__init__(self,id,name,descr)
- 
+
 	def onAdvEvent (self,event,npc,player) :
 		htmltext = event
 		st = player.getQuestState(qn)
@@ -24,6 +35,27 @@ class Quest (JQuest) :
 			st.set("cond","1")
 			st.setState(State.STARTED)
 			st.playSound("ItemSound.quest_accept")
+		elif event == "32258-08.htm" :
+			st.giveItems(PUMA_SKIN_SHIRT, 1)
+			st.giveItems(PUMA_SKIN_GAITERS, 1)
+			st.unset("cond")
+			st.exitQuest(False)
+			st.playSound("ItemSound.quest_finish")
+		elif event == "32258-09.htm" :
+			if st.getRandom(100) < 50 :
+				st.giveItems(MECKLACE_OF_DEVOTION, 1)
+				st.giveItems(RING_OF_DEVOTION, 1)
+			else :
+				st.giveItems(RED_CRESCENT_EARRING, 2)
+			st.unset("cond")
+			st.exitQuest(False)
+			st.playSound("ItemSound.quest_finish")
+		elif event == "quit" :
+			instanceId = player.getInstanceId()
+			instance = InstanceManager.getInstance().getInstance(instanceId)
+			player.setInstanceId(0)
+			player.teleToLocation(-74058,52040,-3680)
+			htmltext = ""
 		return htmltext
 
 	def onTalk (self,npc,player):
@@ -34,27 +66,34 @@ class Quest (JQuest) :
 		npcId = npc.getNpcId()
 		id = st.getState()
 		cond = st.getInt("cond")
-		st1 = player.getQuestState("178_IconicTrinity")
 
 		if id == State.COMPLETED :
-			htmltext = "<html><body>這是已經完成的任務。</body></html>"
+			if npcId == KEKROPUS :
+				htmltext = "<html><body>這是已經完成的任務。</body></html>"
+			elif npcId == MENACING_MACHINE :
+				htmltext = "32258-10.htm"
 		elif id == State.CREATED :
 			if npcId == KEKROPUS and cond == 0 :
-				if not player.getRace().ordinal() != 5 :
-					htmltext = "32138-00.htm"
-					st.exitQuest(1)
-				elif st.getPlayer().getLevel() >= 17 and st1.getState() == State.COMPLETED :
-					htmltext = "32138-01.htm"
+				if player.getRace().ordinal() == 5 :
+					st1 = player.getQuestState("178_IconicTrinity")
+					if st1 and st1.getState() == State.COMPLETED and st.getPlayer().getLevel() >= 17 :
+						htmltext = "32138-01.htm"
+					else :
+						htmltext = "32138-01a.htm"
+						st.exitQuest(1)
 				else :
-					htmltext = "32138-01a.htm"
+					htmltext = "32138-00.htm"
 					st.exitQuest(1)
 		elif id == State.STARTED:
 			if npcId == KEKROPUS and cond == 1 :
 				htmltext = "32138-05.htm"
+			elif npcId == MENACING_MACHINE and cond == 1 :
+				htmltext = "32258-01.htm"
 		return htmltext
 
 QUEST		= Quest(179,qn,"進入大空洞")
 
-QUEST.addStartNpc(32138)
+QUEST.addStartNpc(KEKROPUS)
 
-QUEST.addTalkId(32138)
+QUEST.addTalkId(KEKROPUS)
+QUEST.addTalkId(MENACING_MACHINE)
--- L2JTW_Datapack_Gracia_Plus/data/scripts/quests/182_NewRecruits/__init__.py
+++ L2JTW_Datapack_Gracia_Plus/data/scripts/quests/182_NewRecruits/__init__.py
@@ -1,14 +1,21 @@
 # Update By pmq 14-09-2010
 
 import sys
-from com.l2jserver.gameserver.model.quest import State
-from com.l2jserver.gameserver.model.quest import QuestState
-from com.l2jserver.gameserver.model.quest.jython import QuestJython as JQuest
+from com.l2jserver.gameserver.instancemanager      import InstanceManager
+from com.l2jserver.gameserver.model.actor.instance import L2PcInstance
+from com.l2jserver.gameserver.model.entity         import Instance
+from com.l2jserver.gameserver.model.quest          import State
+from com.l2jserver.gameserver.model.quest          import QuestState
+from com.l2jserver.gameserver.model.quest.jython   import QuestJython as JQuest
 
 qn = "182_NewRecruits"
 
 #NPC'S
-KEKROPUS = 32138
+KEKROPUS             = 32138 # 長老 凱克洛普斯
+MENACING_MACHINE     = 32258 # 巨大機械裝置
+#ITEM
+RED_CRESCENT_EARRING = 10122 # 紅新月耳環
+RING_OF_DEVOTION     = 10124 # 奉獻戒指
 
 class Quest (JQuest) :
 
@@ -24,6 +31,22 @@ class Quest (JQuest) :
 			st.set("cond","1")
 			st.setState(State.STARTED)
 			st.playSound("ItemSound.quest_accept")
+		elif event == "32258-04.htm" :
+			st.giveItems(RED_CRESCENT_EARRING, 2)
+			st.unset("cond")
+			st.exitQuest(False)
+			st.playSound("ItemSound.quest_finish")
+		elif event == "32258-05.htm" :
+			st.giveItems(RING_OF_DEVOTION, 2)
+			st.unset("cond")
+			st.exitQuest(False)
+			st.playSound("ItemSound.quest_finish")
+		elif event == "quit" :
+			instanceId = player.getInstanceId()
+			instance = InstanceManager.getInstance().getInstance(instanceId)
+			player.setInstanceId(0)
+			player.teleToLocation(-74058,52040,-3680)
+			htmltext = ""
 		return htmltext
 
 	def onTalk (self,npc,player):
@@ -38,24 +61,31 @@ class Quest (JQuest) :
 		classId = int(st.getPlayer().getClassId().getId())
 
 		if id == State.COMPLETED :
-			htmltext = "<html><body>這是已經完成的任務。</body></html>"
+			if npcId == KEKROPUS :
+				htmltext = "<html><body>這是已經完成的任務。</body></html>"
+			elif npcId == MENACING_MACHINE :
+				htmltext = "32258-06.htm"
 		elif id == State.CREATED :
 			if npcId == KEKROPUS and cond == 0 :
-				if player.getRace().ordinal() != 5 :
-					htmltext = "32138-00.htm"
-					st.exitQuest(1)
-				elif st.getPlayer().getLevel() >= 17 and classId in [0,10,18,25,31,38,44,49,53]:
-					htmltext = "32138-01.htm"
+				if not player.getRace().ordinal() == 5 :
+					if st.getPlayer().getLevel() >= 17 and classId in [0,10,18,25,31,38,44,49,53]:
+						htmltext = "32138-01.htm"
+					else :
+						htmltext = "32138-01a.htm"
+						st.exitQuest(1)
 				else :
-					htmltext = "<center><font color=\"FF0000\">（功能尚未實裝！）</font></center>"
+					htmltext = "32138-00.htm"
 					st.exitQuest(1)
 		elif id == State.STARTED:
 			if npcId == KEKROPUS and cond == 1 :
 				htmltext = "32138-04.htm"
+			elif npcId == MENACING_MACHINE and cond == 1 :
+				htmltext = "32258-01.htm"
 		return htmltext
 
 QUEST		= Quest(182,qn,"募集幫手")
 
-QUEST.addStartNpc(32138)
+QUEST.addStartNpc(KEKROPUS)
 
-QUEST.addTalkId(32138)
+QUEST.addTalkId(KEKROPUS)
+QUEST.addTalkId(MENACING_MACHINE)
