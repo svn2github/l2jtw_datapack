b5063c393b890f7187033ab0a549dc8914847ef6
pmq <pmq@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2010-10-15 07:04:34 +0000

    修正一些問題
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@963 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_Gracia_Plus/sql/clanhall_siege.sql
+++ L2JTW_Datapack_Gracia_Plus/sql/clanhall_siege.sql
@@ -7,7 +7,7 @@ CREATE TABLE `clanhall_siege` (
 `siege_date` DOUBLE( 20, 0 ) NOT NULL DEFAULT '0' 
 ) ENGINE = MYISAM ;
 
-INSERT INTO `clanhall_siege` VALUES ('34', '1265857200000');
 INSERT INTO `clanhall_siege` VALUES ('21', '1265857200000');
+INSERT INTO `clanhall_siege` VALUES ('34', '1265857200000');
 INSERT INTO `clanhall_siege` VALUES ('62', '1286676000000');
 INSERT INTO `clanhall_siege` VALUES ('64', '1265943600000');
