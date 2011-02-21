47a95cce8ffb64c3acc27833b14dc8f0ed2a3651
pmq <pmq@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2011-02-21 19:10:24 +0000

    更新任務 109 尋找巢穴
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@1092 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_Gracia_Plus/data/scripts/quests/109_InSearchOfTheNest/__init__.py
+++ L2JTW_Datapack_Gracia_Plus/data/scripts/quests/109_InSearchOfTheNest/__init__.py
@@ -1,4 +1,5 @@
-# Created by Eyerobot, edited by Emperorc
+# Created by pmq
+# High Five 22-02-2011
 import sys
 from com.l2jserver.gameserver.model.quest import State
 from com.l2jserver.gameserver.model.quest import QuestState
@@ -6,86 +7,84 @@ from com.l2jserver.gameserver.model.quest.jython import QuestJython as JQuest
 
 qn = "109_InSearchOfTheNest"
 
-# ~~~~~ npcId list: ~~~~~
-Pierce          = 31553
-Corpse          = 32015
-Kahman          = 31554
-# ~~~~~~~~~~~~~~~~~~~~~~~
+# Npc
+PIERCE  = 31553  # 傭兵隊長 比斯
+CORPSE  = 32015  # 偵查兵的屍體
+KAHMAN  = 31554  # 傭兵 卡姆恩
+# Item
+MEMO    = 8083   # 偵查兵的偵查記錄
 
-# ~~~~~~ itemId list: ~~~~~~
-Memo            = 8083
-Golden_Badge_Recruit = 7246
-Golden_Badge_Soldier = 7247
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+class Quest (JQuest):
 
-class Quest (JQuest) : 
+	def __init__(self,id,name,descr):
+		JQuest.__init__(self,id,name,descr)
+		self.questItemIds = [MEMO]
 
-    def __init__(self,id,name,descr):
-    	JQuest.__init__(self,id,name,descr)
-    	self.questItemIds = [Memo]
+	def onAdvEvent (self,event,npc,player):
+		htmltext = event
+		st = player.getQuestState(qn)
+		if not st: return
 
-    def onAdvEvent (self,event,npc,player) :
-        st = player.getQuestState(qn)
-        if not st: return
-        htmltext = event
-        cond = st.getInt("cond")
-        if event == "Memo" and cond == 1 :
-            st.giveItems(Memo,1)
-            st.set("cond","2")
-            st.playSound("ItemSound.quest_itemget")
-            return
-        elif event == "31553-02.htm" and cond == 2 :
-            st.takeItems(Memo,-1)
-            st.set("cond","3")
-            st.playSound("ItemSound.quest_middle") 
-        return htmltext
+		if event == "31553-02.htm":
+			st.set("cond","1")
+			st.setState(State.STARTED)
+			st.playSound("ItemSound.quest_accept")
+		elif event == "32015-02.htm":
+			st.giveItems(MEMO,1)
+			st.set("cond","2")
+			st.playSound("ItemSound.quest_itemget")
+		elif event == "31553-05.htm":
+			st.takeItems(MEMO,-1)
+			st.set("cond","3")
+			st.playSound("ItemSound.quest_middle")
+		elif event == "31554-02.htm":
+			st.giveItems(57,161500)
+			st.addExpAndSp(701500,50000)
+			st.unset("cond")
+			st.exitQuest(False)
+			st.playSound("ItemSound.quest_finish")
+		return htmltext
 
-    def onTalk (self,npc,player):
-        htmltext = "<html><body>目前沒有執行任務，或條件不符。</body></html>"
-        st = player.getQuestState(qn)
-        if st :
-            npcId = npc.getNpcId()
-            cond = st.getInt("cond")
-            state = st.getState()
-            if state == State.COMPLETED :
-                htmltext = "<html><body>這是已經完成的任務。</body></html>"
-            elif state == State.CREATED :
-                if st.getPlayer().getLevel() >= 66 and npcId == Pierce and (st.getQuestItemsCount(Golden_Badge_Recruit) > 0 or st.getQuestItemsCount(Golden_Badge_Soldier) > 0) :
-                    st.setState(State.STARTED)
-                    st.playSound("ItemSound.quest_accept")
-                    st.set("cond","1")
-                    htmltext = "<html><body>Mercenary Captain Pierce:<br>I sent out a scout a while ago, and he hasn't reported back yet. \
-                    Please follow his trail and discover his fate.</body></html>" #this is custom, if someone knows this html from retail, please contribute it.
-                else :
-                    htmltext = "31553-00.htm"
-                    st.exitQuest(1)
-                    st.playSound("ItemSound.quest_giveup")
-            elif state == State.STARTED :
-                if npcId == Corpse :
-                    if cond == 1 :
-                        htmltext = "32015-01.htm"
-                    elif cond == 2 :
-                        htmltext = "<html><body>This is nothing else here. Maybe you should take that memo to Pierce?</body></html>"#again, custom, see note above
-                elif npcId == Pierce :
-                    if cond == 1 :
-                        htmltext = "<html><body>Mercenary Captain Pierce:<br>Please find my scout!</body></html>"#custom
-                    elif cond == 2 :
-                        htmltext = "31553-01.htm"
-                    elif cond == 3 :
-                        htmltext = "<html><body>Mercenary Captain Pierce:<br>Thanks for your help. See Kahman for your reward!</body></html>"#custom
-                elif npcId == Kahman and cond == 3 :
-                    htmltext = "31554-01.htm"
-                    st.giveItems(57,161500)
-                    st.addExpAndSp(701500,50000)
-                    st.unset("cond")
-                    st.exitQuest(False)
-                    st.playSound("ItemSound.quest_finish")
-        return htmltext 
+	def onTalk(self, npc, player):
+		htmltext = "<html><body>目前沒有執行任務，或條件不符。</body></html>"
+		st = player.getQuestState(qn)
+		if not st :return htmltext
 
-QUEST = Quest(109,qn,"尋找巢穴")
+		npcId = npc.getNpcId()
+		id = st.getState()
+		cond = st.getInt("cond")
 
-QUEST.addStartNpc(Pierce)
+		if id == State.COMPLETED :
+			htmltext = "<html><body>這是已經完成的任務。</body></html>"
+		elif id == State.CREATED :
+			if npcId == PIERCE :
+				if st.getPlayer().getLevel() >= 81 :
+					htmltext = "31553-01.htm"
+				else :
+					htmltext = "31553-00.htm"
+					st.exitQuest(1)
+		elif id == State.STARTED :
+			if npcId == PIERCE :
+				if cond == 1 :
+					htmltext = "31553-03.htm"
+				elif cond == 2 :
+					htmltext = "31553-04.htm"
+				elif cond == 3 :
+					htmltext = "31553-06.htm"
+			elif npcId == CORPSE :
+				if cond == 1 :
+					htmltext = "32015-01.htm"
+				elif cond == 2 :
+					htmltext = "32015-03.htm"
+			elif npcId == KAHMAN :
+				if cond == 3 :
+					htmltext = "31554-01.htm"
+		return htmltext
 
-QUEST.addTalkId(Pierce)
-QUEST.addTalkId(Corpse)
-QUEST.addTalkId(Kahman)
+QUEST		= Quest(109,qn,"尋找巢穴")
+
+QUEST.addStartNpc(PIERCE)
+
+QUEST.addTalkId(PIERCE)
+QUEST.addTalkId(CORPSE)
+QUEST.addTalkId(KAHMAN)
