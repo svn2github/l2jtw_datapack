2a8dceed932bef80aaa1fc5b9945493685697b67
rocknow <rocknow@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2011-04-20 10:28:04 +0000

    Highfive sync 7912 - sql (未完成)
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@1155 9bd44e3e-6552-0410-907e-a6ca40b856b6

---
+++ L2JTW_Datapack_HighFive/sql/L2JTW_2/auto_chat_text_tw.sql
@@ -0,0 +1,23 @@
+Set Names utf8;
+/************ Made in Taiwan ************/
+
+/***** GAME *****/ 
+/***** auto_chat_text.sql *****/
+UPDATE `auto_chat_text` SET `chatText` = '%player_cabal_loser%！大家等著絕望吧！死亡女神即將要降臨了！' WHERE `chatText` = '%player_cabal_loser%! All is lost! Prepare to meet the goddess of death!';
+UPDATE `auto_chat_text` SET `chatText` = '%player_cabal_loser%！你已經遭遇不幸！' WHERE `chatText` = '%player_cabal_loser%! You bring an ill wind!';
+UPDATE `auto_chat_text` SET `chatText` = '%player_cabal_loser%！還不如快點放棄！' WHERE `chatText` = '%player_cabal_loser%! You might as well give up!';
+UPDATE `auto_chat_text` SET `chatText` = '失敗者將遭受詛咒！' WHERE `chatText` = 'A curse upon you!';
+UPDATE `auto_chat_text` SET `chatText` = '大家等著絕望吧！死亡女神即將要降臨了！' WHERE `chatText` = 'All is lost! Prepare to meet the goddess of death!';
+UPDATE `auto_chat_text` SET `chatText` = '大家等著絕望吧！滅亡的預言即將就要實現了！' WHERE `chatText` = 'All is lost! The prophecy of destruction has been fulfilled!';
+UPDATE `auto_chat_text` SET `chatText` = '聆聽滅亡的預言吧！末日時代即將要來臨了！' WHERE `chatText` = 'The prophecy of doom has awoken!';
+UPDATE `auto_chat_text` SET `chatText` = '這個世界即將滅亡！' WHERE `chatText` = 'This world will soon be annihilated!';
+UPDATE `auto_chat_text` SET `chatText` = '%player_cabal_winner%！施予你啟示的祝福！' WHERE `chatText` = '%player_cabal_winner%! I bestow on you the authority of the abyss!';
+UPDATE `auto_chat_text` SET `chatText` = '%player_cabal_winner%！黑暗將會被放逐！' WHERE `chatText` = '%player_cabal_winner%, Darkness shall be banished forever!';
+UPDATE `auto_chat_text` SET `chatText` = '%player_cabal_winner%！榮耀時刻就在眼前！' WHERE `chatText` = '%player_cabal_winner%, the time for glory is at hand!';
+UPDATE `auto_chat_text` SET `chatText` = '迎接勝利的到來吧！' WHERE `chatText` = 'All hail the eternal twilight!';
+UPDATE `auto_chat_text` SET `chatText` = '聆聽黑暗的啟示吧！混沌的時代已經開始了！' WHERE `chatText` = 'As foretold in the prophecy of darkness, the era of chaos has begun!';
+UPDATE `auto_chat_text` SET `chatText` = '審判的日子即將來臨！' WHERE `chatText` = 'The day of judgment is near!';
+UPDATE `auto_chat_text` SET `chatText` = '聆聽黑暗的啟示吧！死亡的樂園即將來臨了！' WHERE `chatText` = 'The prophecy of darkness has been fulfilled!';
+UPDATE `auto_chat_text` SET `chatText` = '聆聽黑暗的啟示吧！沉睡中的黑暗甦醒了！' WHERE `chatText` = 'The prophecy of darkness has come to pass!';
+
+/***** Done by vdmyagami！ *****/
