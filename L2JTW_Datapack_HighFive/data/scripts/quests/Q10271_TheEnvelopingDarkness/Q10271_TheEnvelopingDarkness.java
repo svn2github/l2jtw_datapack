c98b890651ef2df9d6c60521b93c0c0e1c4bcb14
pmq <pmq@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2011-04-26 03:26:01 +0000

    High Five sync 7941 & fix
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@1171 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_HighFive/data/scripts/quests/Q10271_TheEnvelopingDarkness/Q10271_TheEnvelopingDarkness.java
+++ L2JTW_Datapack_HighFive/data/scripts/quests/Q10271_TheEnvelopingDarkness/Q10271_TheEnvelopingDarkness.java
@@ -16,123 +16,109 @@ public class Q10271_TheEnvelopingDarkness extends Quest
 	// Npc
 	private static final int ORBYU = 32560;
 	private static final int EL = 32556;
-  private static final int MEDIBAL_CORPSE = 32528;
-  // Item
-  private static final int MEDIBAL_DOCUMENT = 13852;
+	private static final int MEDIBAL_CORPSE = 32528;
+	// Item
+	private static final int MEDIBAL_DOCUMENT = 13852;
 
-
-   @Override
+	@Override
 	public String onTalk(L2Npc npc, L2PcInstance player)
-  {
+	{
 		String htmltext = getNoQuestMsg(player);
 		QuestState st =  player.getQuestState(qn);
-    if (st == null)
+		
+		if (st == null)
 			return htmltext;
 
-    if (npc.getNpcId() == ORBYU)
-    {
-     switch (st.getState())
-      {
-      // Vytvoření questu pokud hráč splňuje podmínku min. lvlu 75 + kompletní quest To the Seed of Desctruction
-			case State.CREATED:
-      QuestState _prev = player.getQuestState("10269_ToTheSeedOfDestruction");
-      if ((_prev != null) && (_prev.getState() == State.COMPLETED) && (player.getLevel() >= 75))
-					htmltext = "32560-01.htm";
-        else 
-          htmltext = "32560-02.htm"; 
-		        break;
-            // HTML při přijmutí questu       
-			case State.STARTED:
+		if (npc.getNpcId() == ORBYU)
+		{
+			switch (st.getState())
+			{
+				case State.CREATED:
+					QuestState _prev = player.getQuestState("10269_ToTheSeedOfDestruction");
+					if ((_prev != null) && (_prev.getState() == State.COMPLETED) && (player.getLevel() >= 75))
+						htmltext = "32560-01.htm";
+					else 
+						htmltext = "32560-02.htm"; 
+						break;
+				case State.STARTED:
 					htmltext = "32560-05.htm";
-          break;
-      case State.COMPLETED:
+					break;
+				case State.COMPLETED:
 					htmltext = "32560-03.htm";
-          break;   
-      }
-         // HTML při kondici 2
-    if (st.getInt("cond") == 2)
-         {
-      htmltext = "32560-06.htm";
-         }
-         // HTML při kondici 3
-     else if (st.getInt("cond") == 3)
-         {
-      htmltext = "32560-07.htm";
-         }
-     else if (st.getInt("cond") == 4)
-         {
-      htmltext = "32560-08.htm";
-      st.unset("cond");
-      st.setState(State.COMPLETED);
-      st.giveItems(57, 62516);
-      st.addExpAndSp(377403, 37867);
-      st.playSound("ItemSound.quest_finish");
-			st.exitQuest(false);
-         }         
-         
-     }
-     
-     else if (npc.getNpcId() == EL)
-       { 
-       switch (st.getState())
-      {  
-       case State.COMPLETED:
+					break;
+			}
+			if (st.getInt("cond") == 2)
+			{
+				htmltext = "32560-06.htm";
+			}
+			else if (st.getInt("cond") == 3)
+			{
+				htmltext = "32560-07.htm";
+			}
+			else if (st.getInt("cond") == 4)
+			{
+				htmltext = "32560-08.htm";
+				st.unset("cond");
+				st.setState(State.COMPLETED);
+				st.giveItems(57, 62516);
+				st.addExpAndSp(377403, 37867);
+				st.playSound("ItemSound.quest_finish");
+				st.exitQuest(false);
+			}
+		}
+		else if (npc.getNpcId() == EL)
+		{
+			switch (st.getState())
+			{
+				case State.COMPLETED:
 					htmltext = "32556-02.htm";
-          break;   
-      }
-       // HTML při kondici 1  
-				    if (st.getInt("cond") == 1)
-        		{
-        		  htmltext = "32556-01.htm";
-        		}
-            // HTML při kondici 2
-        		else if (st.getInt("cond") == 2)
-        		{
-					    htmltext = "32556-07.htm";
-        		}
-            // HTML při kondici 3
-            else if (st.getInt("cond") == 3)
-        		{
-        		  htmltext = "32556-08.htm";
-        		}
-            // HTML při kondici 4
-            else if (st.getInt("cond") == 4)
-        		{
-        		  htmltext = "32556-09.htm";
-        		}
-		   }
-       
-       else if (npc.getNpcId() == MEDIBAL_CORPSE)
-       { 
-       switch (st.getState())
-      {  
-       case State.COMPLETED:
+					break;
+			}
+			if (st.getInt("cond") == 1)
+			{
+				htmltext = "32556-01.htm";
+			}
+			else if (st.getInt("cond") == 2)
+			{
+				htmltext = "32556-07.htm";
+			}
+			else if (st.getInt("cond") == 3)
+			{
+				htmltext = "32556-08.htm";
+			}
+			else if (st.getInt("cond") == 4)
+			{
+				htmltext = "32556-09.htm";
+			}
+		}
+		else if (npc.getNpcId() == MEDIBAL_CORPSE)
+		{
+			switch (st.getState())
+			{
+				case State.COMPLETED:
 					htmltext = "32528-02.htm";
-          break;   
-      }  
-       // HTML při kondici 2 
-       if (st.getInt("cond") == 2)
-        		{
-        		   htmltext = "32528-01.htm";
-               st.playSound("ItemSound.quest_middle");
-               st.set("cond", "3");
-               st.giveItems(MEDIBAL_DOCUMENT, 1);
-        		} 
-         // HTML při kondici 3   
-				else if (st.getInt("cond") == 3)
-        		{
-        		  htmltext = "32528-03.htm";
-        		}
-             // HTML při kondici 4   
-				else if (st.getInt("cond") == 4)
-        		{
-        		  htmltext = "32528-03.htm";
-        		}   
-		   }
-       	return htmltext;
-  }
-      
-   	@Override
+					break;
+			}  
+			if (st.getInt("cond") == 2)
+			{
+				htmltext = "32528-01.htm";
+				st.playSound("ItemSound.quest_middle");
+				st.set("cond", "3");
+				st.giveItems(MEDIBAL_DOCUMENT, 1);
+			} 
+			else if (st.getInt("cond") == 3)
+			{
+				htmltext = "32528-03.htm";
+			}
+			else if (st.getInt("cond") == 4)
+			{
+				htmltext = "32528-03.htm";
+			}
+		}
+		return htmltext;
+	}
+
+	@Override
 	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
 	{
 		String htmltext = event;
@@ -140,43 +126,39 @@ public class Q10271_TheEnvelopingDarkness extends Quest
 		
 		if (st == null)
 			return htmltext;
-		// Quest přijmut + zvuk přijmutí questu
+
 		if (event.equalsIgnoreCase("32560-05.htm"))
-		   {
+		{
 			st.setState(State.STARTED);
 			st.set("cond", "1");
 			st.playSound("ItemSound.quest_accept");
-		   }
-       // Quest update + zvuk updatu questu
-	  else	if (event.equalsIgnoreCase("32556-06.htm"))
-		   {
+		}
+		else if (event.equalsIgnoreCase("32556-06.htm"))
+		{
 			st.set("cond", "2");
 			st.playSound("ItemSound.quest_middle");
-		   }
-       // Quest update + zvuk updatu questu
-       else	if (event.equalsIgnoreCase("32556-09.htm"))
-		   {
+		}
+		else if (event.equalsIgnoreCase("32556-09.htm"))
+		{
 			st.set("cond", "4");
 			st.playSound("ItemSound.quest_middle");
-      st.takeItems(MEDIBAL_DOCUMENT, 1);
-      
-		   }    
+			st.takeItems(MEDIBAL_DOCUMENT, 1);
+		}
 		return htmltext;
-	}  
-          
-    
+	}
+
 	public Q10271_TheEnvelopingDarkness(int questId, String name, String descr)
 	{
 		super(questId, name, descr);
 		addStartNpc(ORBYU);
 		addTalkId(ORBYU);
-    addTalkId(EL);
-    addTalkId(MEDIBAL_CORPSE);
-    questItemIds = new int[] {MEDIBAL_DOCUMENT};
+		addTalkId(EL);
+		addTalkId(MEDIBAL_CORPSE);
+		questItemIds = new int[] {MEDIBAL_DOCUMENT};
 	}
-  
+
 	public static void main(String[] args)
 	{
-		new Q10271_TheEnvelopingDarkness(10271, qn, "The Enveloping Darkness");
+		new Q10271_TheEnvelopingDarkness(10271, qn, "Ţ�n���·t");
 	}
 }
--- L2JTW_Datapack_HighFive/data/scripts/quests/Q10272_LightFragment/Q10272_LightFragment.java
+++ L2JTW_Datapack_HighFive/data/scripts/quests/Q10272_LightFragment/Q10272_LightFragment.java
@@ -18,230 +18,201 @@ public class Q10272_LightFragment extends Quest
 	// Npc
 	private static final int ORBYU = 32560;
 	private static final int ARTIUS = 32559;
-  private static final int GINBY = 32566;
-  private static final int LELRIKIA = 32567;
-  private static final int LEKON = 32557;
-  // Příšery 
-  private static final int[] Monsters = {22536, 22537, 22538, 22539, 22540, 22541, 22542, 22543, 22544, 22547, 22550, 22551, 22552, 22596};
-  // Itemy
-  private static final int FRAGMENT_POWDER = 13853;
-  private static final int LIGHT_FRAGMENT_POWDER = 13854;
-  private static final int ADENA = 57;
-  // Drop šance
-  private static final double DROP_CHANCE = 90;
-  // Zóna pro výměnu itemů 
-//  private static final int ZONE = 88888;
+	private static final int GINBY = 32566;
+	private static final int LELRIKIA = 32567;
+	private static final int LEKON = 32557;
+	// Mobs
+	private static final int[] Monsters = {22536, 22537, 22538, 22539, 22540, 22541, 22542, 22543, 22544, 22547, 22550, 22551, 22552, 22596};
+	// Item
+	private static final int FRAGMENT_POWDER = 13853;
+	private static final int LIGHT_FRAGMENT_POWDER = 13854;
+	private static final int ADENA = 57;
+	// Drop Chance
+	private static final double DROP_CHANCE = 90;
+	// Zone 
+	//private static final int ZONE = 88888;
 
-   @Override
+	@Override
 	public String onTalk(L2Npc npc, L2PcInstance player)
-  {
+	{
 		String htmltext = getNoQuestMsg(player);
 		QuestState st =  player.getQuestState(qn);
-    if (st == null)
+
+		if (st == null)
 			return htmltext;
 
-    if (npc.getNpcId() == ORBYU)
-    {
-     switch (st.getState())
-      {
-      // Vytvoření questu pokud hráč splňuje podmínku min. lvlu 75 + kompletní quest 10271_TheEnvelopingDarkness
-			case State.CREATED:
-      QuestState _prev = player.getQuestState("10271_TheEnvelopingDarkness");
-      if ((_prev != null) && (_prev.getState() == State.COMPLETED) && (player.getLevel() >= 75))
-					htmltext = "32560-01.htm";
-        else 
-          htmltext = "32560-02.htm"; 
-     if (player.getLevel() <= 75)
-          htmltext = "32560-03.htm"; 
-		        break;
-            // HTML při přijmutí questu       
-			case State.STARTED:
+		if (npc.getNpcId() == ORBYU)
+		{
+			switch (st.getState())
+			{
+				case State.CREATED:
+					QuestState _prev = player.getQuestState("10271_TheEnvelopingDarkness");
+					if ((_prev != null) && (_prev.getState() == State.COMPLETED) && (player.getLevel() >= 75))
+						htmltext = "32560-01.htm";
+					else 
+						htmltext = "32560-02.htm"; 
+						if (player.getLevel() <= 75)
+							htmltext = "32560-03.htm"; 
+					break;
+				case State.STARTED:
 					htmltext = "32560-06.htm";
-          break;
-      case State.COMPLETED:
+					break;
+				case State.COMPLETED:
 					htmltext = "32560-04.htm";
-          break;   
-      }
-      
-     if (st.getInt("cond") == 2)
-         {
-      htmltext = "32560-06.htm";
-         }         
-     }
-     
-     else if (npc.getNpcId() == ARTIUS)
-       { 
-       switch (st.getState())
-      {  
-       case State.COMPLETED:
+					break;
+			}
+			if (st.getInt("cond") == 2)
+			{
+				htmltext = "32560-06.htm";
+			}
+		}
+		else if (npc.getNpcId() == ARTIUS)
+		{ 
+			switch (st.getState())
+			{
+				case State.COMPLETED:
 					htmltext = "32559-19.htm";
-          break;   
-      }
-       // HTML při kondici 1  
-				    if (st.getInt("cond") == 1)
-        		{
-        		  htmltext = "32559-01.htm";
-        		}
-            // HTML při kondici 2  
-				    if (st.getInt("cond") == 2)
-        		{
-        		  htmltext = "32559-04.htm";
-        		}
-            // HTML při kondici 3  
-				    if (st.getInt("cond") == 3)
-        		{
-        		  htmltext = "32559-08.htm";
-        		}
-             // HTML při kondici 4  
-				    else if (st.getInt("cond") == 4)
-        		{
-        		  htmltext = "32559-10.htm";
-        		}
-             // HTML při kondici 5  
-				    else if (st.getInt("cond") == 5)
-        		{
-            if (st.getQuestItemsCount(FRAGMENT_POWDER) >= 100)
-              {
-        		  htmltext = "32559-15.htm";
-              st.set("cond", "6");
-              }
-            else if (st.getQuestItemsCount(FRAGMENT_POWDER) >= 1)
-              {
-              htmltext = "32559-14.htm";
-              }
-           else if (st.getQuestItemsCount(FRAGMENT_POWDER) < 1)
-               {
-              htmltext = "32559-13.htm"; 
-        		   }
-             }  
-             // HTML při kondici 6  
-				    else if (st.getInt("cond") == 6)
-        		  {
-        		  htmltext = "32559-16.htm";
-        		  }
-          }   
-       
-         else if (npc.getNpcId() == GINBY)
-       { 
-       switch (st.getState())
-        {  
-       case State.COMPLETED:
+					break;
+			}
+			if (st.getInt("cond") == 1)
+			{
+				htmltext = "32559-01.htm";
+			}
+			if (st.getInt("cond") == 2)
+			{
+				htmltext = "32559-04.htm";
+			}
+			if (st.getInt("cond") == 3)
+			{
+				htmltext = "32559-08.htm";
+			}
+			else if (st.getInt("cond") == 4)
+			{
+				htmltext = "32559-10.htm";
+			}
+			else if (st.getInt("cond") == 5)
+			{
+				if (st.getQuestItemsCount(FRAGMENT_POWDER) >= 100)
+				{
+					htmltext = "32559-15.htm";
+					st.set("cond", "6");
+				}
+				else if (st.getQuestItemsCount(FRAGMENT_POWDER) >= 1)
+				{
+					htmltext = "32559-14.htm";
+				}
+				else if (st.getQuestItemsCount(FRAGMENT_POWDER) < 1)
+				{
+					htmltext = "32559-13.htm"; 
+				}
+			}
+			else if (st.getInt("cond") == 6)
+			{
+				htmltext = "32559-16.htm";
+			}
+		}
+		else if (npc.getNpcId() == GINBY)
+		{ 
+			switch (st.getState())
+			{  
+				case State.COMPLETED:
 					htmltext = "32559-19.htm";
-          break;   
-         }
-            // HTML při kondici 1  
-				    if (st.getInt("cond") == 1)
-        		{
-        		  htmltext = "32566-02.htm";
-        		}
-            // HTML při kondici 2  
-				    else if (st.getInt("cond") == 2)
-        		{
-        		  htmltext = "32566-02.htm";
-        		}
-             // HTML při kondici 3  
-				    else if (st.getInt("cond") == 3)
-        		{
-        		  htmltext = "32566-01.htm";
-        		}
-              // HTML při kondici 4  
-				    else if (st.getInt("cond") == 4)
-        		{
-        		  htmltext = "32566-09.htm";
-        		}
-             // HTML při kondici 5  
-				    else if (st.getInt("cond") == 5)
-        		{ 
-        		  htmltext = "32566-10.htm";
-        		}
-            // HTML při kondici 6
-             else if (st.getInt("cond") == 6)
-        		{ 
-        		  htmltext = "32566-10.htm";
-        		}
-            
-		   } 
-       
-        else if (npc.getNpcId() == LELRIKIA)
-         { 
-             // HTML při kondici 3  
-				    if (st.getInt("cond") == 3)
-        		{
-        		  htmltext = "32567-01.htm";
-        		}
-              // HTML při kondici 4  
-				    else if (st.getInt("cond") == 4)
-        		{
-        		  htmltext = "32567-05.htm";
-        		}
-		      }       
-             
-       	return htmltext;
-  }
-      
-   	@Override
+					break;
+			}
+			if (st.getInt("cond") == 1)
+			{
+				htmltext = "32566-02.htm";
+			}
+			else if (st.getInt("cond") == 2)
+			{
+				htmltext = "32566-02.htm";
+			}
+			else if (st.getInt("cond") == 3)
+			{
+				htmltext = "32566-01.htm";
+			}
+			else if (st.getInt("cond") == 4)
+			{
+				htmltext = "32566-09.htm";
+			}
+			else if (st.getInt("cond") == 5)
+			{ 
+				htmltext = "32566-10.htm";
+			}
+			else if (st.getInt("cond") == 6)
+			{ 
+				htmltext = "32566-10.htm";
+			}
+		} 
+		else if (npc.getNpcId() == LELRIKIA)
+		{ 
+			if (st.getInt("cond") == 3)
+			{
+				htmltext = "32567-01.htm";
+			}
+			else if (st.getInt("cond") == 4)
+			{
+				htmltext = "32567-05.htm";
+			}
+		}       
+		return htmltext;
+	}
+
+	@Override
 	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
 	{
 		String htmltext = event;
 		QuestState st = player.getQuestState(qn);
-		
+
 		if (st == null)
 			return htmltext;
-		// Quest přijmut + zvuk přijmutí questu
+
 		if (event.equalsIgnoreCase("32560-06.htm"))
-		   {
+		{
 			st.setState(State.STARTED);
 			st.set("cond", "1");
 			st.playSound("ItemSound.quest_accept");
-		   }
-       // Quest update + zvuk updatu questu
-	  else	if (event.equalsIgnoreCase("32559-03.htm"))
-		   {
+		}
+		else if (event.equalsIgnoreCase("32559-03.htm"))
+		{
 			st.set("cond", "2");
 			st.playSound("ItemSound.quest_middle");
-		   }
-         // Quest update + zvuk updatu questu
-	  else	if (event.equalsIgnoreCase("32559-07.htm"))
-		   {
+		}
+		else if (event.equalsIgnoreCase("32559-07.htm"))
+		{
 			st.set("cond", "3");
+			st.playSound("ItemSound.quest_middle");
+		}
+		else if (event.equalsIgnoreCase("pay"))
+		{
+			if (st.getQuestItemsCount(ADENA) >= 10000)
+				st.takeItems(ADENA, 10000);
+				htmltext = "32566-05.htm";
+			if (st.getQuestItemsCount(ADENA) < 10000)
+				htmltext = "32566-04a.htm";
+		}
+		else if (event.equalsIgnoreCase("32567-04.htm"))
+		{
+			st.set("cond", "4");
 			st.playSound("ItemSound.quest_middle"); 
-       }
-        // Zaplacení aden a změnění html 
-    else	if (event.equalsIgnoreCase("pay"))
-		   {
-        if (st.getQuestItemsCount(ADENA) >= 10000)
-              st.takeItems(ADENA, 10000);
-              htmltext = "32566-05.htm";
-        if (st.getQuestItemsCount(ADENA) < 10000)
-              htmltext = "32566-04a.htm";
-       }
-           // Quest update + zvuk updatu questu
-	  else	if (event.equalsIgnoreCase("32567-04.htm"))
-		    {
-			        st.set("cond", "4");
-			        st.playSound("ItemSound.quest_middle"); 
-        }
-               // Quest update + zvuk updatu questu
-	  else	if (event.equalsIgnoreCase("32559-12.htm"))
-		    { 
-			        st.set("cond", "5");
-			        st.playSound("ItemSound.quest_middle"); 
-        }
-        
-          
+		}
+		else if (event.equalsIgnoreCase("32559-12.htm"))
+		{ 
+			st.set("cond", "5");
+			st.playSound("ItemSound.quest_middle"); 
+		}
 		return htmltext;
-  }  
-            
-  	@Override
+	}
+
+	@Override
 	public final String onKill(L2Npc npc, L2PcInstance player, boolean isPet)
 	{
-   // Při kondici 5 se budou dropovat quest itemy
 		final QuestState st = player.getQuestState(qn);
 		if (st.getInt("cond") == 5)
 		{
 			final long count = st.getQuestItemsCount(FRAGMENT_POWDER);
 			if (count < 100)
-			{  // Rate quest dropu ovliňuje config
+			{
 				int chance = (int)(Config.RATE_QUEST_DROP * DROP_CHANCE);
 				int numItems = chance / 100;
 				chance = chance % 100;
@@ -249,13 +220,11 @@ public class Q10272_LightFragment extends Quest
 					numItems++;
 				if (numItems > 0)
 				{ 
-         // Při 100x FRAGMENT_POWDER se už nebudou dropovat další quest itemy
 					if (count + numItems >= 100)
 					{
 						numItems = 100 - (int)count;
 						st.playSound("ItemSound.quest_middle");
 					}
-         // Pokud hráč nemá 100x FRAGMENT_POWDER dropují se itemy dál
 					else
 						st.playSound("ItemSound.quest_itemget");
 						st.giveItems(FRAGMENT_POWDER, numItems);
@@ -263,29 +232,27 @@ public class Q10272_LightFragment extends Quest
 			}
 		}
 		return null;
-	} 
-  
-   
-    
+	}
+
 	public Q10272_LightFragment(int questId, String name, String descr)
 	{
 		super(questId, name, descr);
 		addStartNpc(ORBYU);
 		addTalkId(ORBYU);
-    addTalkId(ARTIUS);
-    addTalkId(GINBY);
-    addTalkId(LELRIKIA);
-    addTalkId(LEKON);
-    for (int i : Monsters)
+		addTalkId(ARTIUS);
+		addTalkId(GINBY);
+		addTalkId(LELRIKIA);
+		addTalkId(LEKON);
+		for (int i : Monsters)
 		{
 			addKillId(i);
 		}
-  //  addEnterZoneId(ZONE);
+		//addEnterZoneId(ZONE);
 		questItemIds = new int[] {FRAGMENT_POWDER,LIGHT_FRAGMENT_POWDER};
 	}
-  
+
 	public static void main(String[] args)
 	{
-		new Q10272_LightFragment(10272, qn, "Light Fragment");
+		new Q10272_LightFragment(10272, qn, "光之破片");
 	}
 }
