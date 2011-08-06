7051126c62a04fe70016cdcbe6ef76bf6e132e2f
pmq <pmq@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2011-08-06 11:54:09 +0000

    High Five sync 8255 ( 需要配合GS - 744 )
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@1313 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_HighFive/data/scripts/handlers/admincommandhandlers/AdminMammon.java
+++ L2JTW_Datapack_HighFive/data/scripts/handlers/admincommandhandlers/AdminMammon.java
@@ -20,37 +20,33 @@ import com.l2jserver.gameserver.model.AutoSpawnHandler;
 import com.l2jserver.gameserver.model.AutoSpawnHandler.AutoSpawnInstance;
 import com.l2jserver.gameserver.model.actor.L2Npc;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
+import com.l2jserver.gameserver.network.SystemMessageId;
 import com.l2jserver.gameserver.datatables.MessageTable;
 
 /**
  * Admin Command Handler for Mammon NPCs
- *
- * @author Tempy
- * Typo fix by Zoey76 24/02/2011
+ * @author Tempy, Zoey76
  */
 public class AdminMammon implements IAdminCommandHandler
 {
-	
-	private static final String[] ADMIN_COMMANDS =
-	{
-		"admin_mammon_find",
-		"admin_mammon_respawn",
-	};
+	private static final String[] ADMIN_COMMANDS = { "admin_mammon_find", "admin_mammon_respawn", };
 	
 	private boolean _isSealValidation = SevenSigns.getInstance().isSealValidationPeriod();
 	
 	public boolean useAdminCommand(String command, L2PcInstance activeChar)
 	{
 		int teleportIndex = -1;
-		AutoSpawnInstance blackSpawnInst = AutoSpawnHandler.getInstance().getAutoSpawnInstance(SevenSigns.MAMMON_BLACKSMITH_ID, false);
-		AutoSpawnInstance merchSpawnInst = AutoSpawnHandler.getInstance().getAutoSpawnInstance(SevenSigns.MAMMON_MERCHANT_ID, false);
+		final AutoSpawnInstance blackSpawnInst = AutoSpawnHandler.getInstance().getAutoSpawnInstance(SevenSigns.MAMMON_BLACKSMITH_ID, false);
+		final AutoSpawnInstance merchSpawnInst = AutoSpawnHandler.getInstance().getAutoSpawnInstance(SevenSigns.MAMMON_MERCHANT_ID, false);
 		
 		if (command.startsWith("admin_mammon_find"))
 		{
 			try
 			{
 				if (command.length() > 17)
+				{
 					teleportIndex = Integer.parseInt(command.substring(18));
+				}
 			}
 			catch (Exception NumberFormatException)
 			{
@@ -60,59 +56,75 @@ public class AdminMammon implements IAdminCommandHandler
 			
 			if (!_isSealValidation)
 			{
-				activeChar.sendMessage(1738);
+				activeChar.sendPacket(SystemMessageId.SSQ_COMPETITION_UNDERWAY);
+				//activeChar.sendMessage(1738);
 				return false;
 			}
 			
 			if (blackSpawnInst != null)
 			{
-				L2Npc[] blackInst = blackSpawnInst.getNPCInstanceList();
+				final L2Npc[] blackInst = blackSpawnInst.getNPCInstanceList();
 				if (blackInst.length > 0)
 				{
-					int x1 = blackInst[0].getX(), y1 = blackInst[0].getY(), z1 = blackInst[0].getZ();
+					final int x1 = blackInst[0].getX(), y1 = blackInst[0].getY(), z1 = blackInst[0].getZ();
 					activeChar.sendMessage(MessageTable.Messages[1739].getMessage() + x1 + " " + y1 + " " + z1);
 					if (teleportIndex == 1)
+					{
 						activeChar.teleToLocation(x1, y1, z1, true);
+					}
 				}
 			}
 			else
+			{
 				activeChar.sendMessage(1740);
+			}
 			
 			if (merchSpawnInst != null)
 			{
-				L2Npc[] merchInst = merchSpawnInst.getNPCInstanceList();
+				final L2Npc[] merchInst = merchSpawnInst.getNPCInstanceList();
 				if (merchInst.length > 0)
 				{
-					int x2 = merchInst[0].getX(), y2 = merchInst[0].getY(), z2 = merchInst[0].getZ();
+					final int x2 = merchInst[0].getX(), y2 = merchInst[0].getY(), z2 = merchInst[0].getZ();
 					activeChar.sendMessage(MessageTable.Messages[1741].getMessage() + x2 + " " + y2 + " " + z2);
 					if (teleportIndex == 2)
+					{
 						activeChar.teleToLocation(x2, y2, z2, true);
+					}
 				}
 			}
 			else
+			{
 				activeChar.sendMessage(1742);
+			}
 		}
 		else if (command.startsWith("admin_mammon_respawn"))
 		{
 			if (!_isSealValidation)
 			{
-				activeChar.sendMessage(1738);
+				activeChar.sendPacket(SystemMessageId.SSQ_COMPETITION_UNDERWAY);
+				//activeChar.sendMessage(1738);
 				return true;
 			}
+			
 			if (merchSpawnInst != null)
 			{
 				long merchRespawn = AutoSpawnHandler.getInstance().getTimeToNextSpawn(merchSpawnInst);
 				activeChar.sendMessage(MessageTable.Messages[1743].getExtra(1) + (merchRespawn / 60000) + MessageTable.Messages[1743].getExtra(2));
 			}
 			else
+			{
 				activeChar.sendMessage(1742);
+			}
+			
 			if (blackSpawnInst != null)
 			{
 				long blackRespawn = AutoSpawnHandler.getInstance().getTimeToNextSpawn(blackSpawnInst);
 				activeChar.sendMessage(MessageTable.Messages[1744].getExtra(1) + (blackRespawn / 60000) + MessageTable.Messages[1744].getExtra(2));
 			}
 			else
+			{
 				activeChar.sendMessage(1740);
+			}
 		}
 		return true;
 	}
