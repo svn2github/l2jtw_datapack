d9451003c2b86ea68efd1ea73a1476cb47a295fc
rocknow <rocknow@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2011-08-28 21:00:31 +0000

    增加版本限制的提示
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@1338 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_HighFive/dist/sql/L2JTW/messagetable.sql
+++ L2JTW_Datapack_HighFive/dist/sql/L2JTW/messagetable.sql
@@ -19,7 +19,12 @@ CREATE TABLE IF NOT EXISTS `messagetable` (
 INSERT INTO `messagetable` VALUES ('0', 'en', 'Delete', '', 'Announcements.java');
 INSERT INTO `messagetable` VALUES ('0', 'tw', '刪除', '', 'Announcements.java');
 INSERT INTO `messagetable` VALUES ('0', 'cn', '删除', '', 'Announcements.java');
-
+INSERT INTO `messagetable` VALUES ('1', 'en', ' Wrong protocol!', '', 'ProtocolVersion.java');
+INSERT INTO `messagetable` VALUES ('1', 'tw', ' 使用「芙蕾雅」登入「嗨翻天」的伺服器，已拒絕。', '', 'ProtocolVersion.java');
+INSERT INTO `messagetable` VALUES ('1', 'cn', ' 使用「芙蕾雅」登入「嗨翻天」的服务器，已拒绝。', '', 'ProtocolVersion.java');
+INSERT INTO `messagetable` VALUES ('2', 'en', 'Wrong protocol!', '', 'ProtocolVersion.java');
+INSERT INTO `messagetable` VALUES ('2', 'tw', ' 使用「毀滅女神」登入「嗨翻天」的伺服器，已拒絕。', '', 'ProtocolVersion.java');
+INSERT INTO `messagetable` VALUES ('2', 'cn', ' 使用「毁灭女神」登入「嗨翻天」的服务器，已拒绝。', '', 'ProtocolVersion.java');
 INSERT INTO `messagetable` VALUES ('3', 'en', 'Manufacturer is busy, please try later.', '', 'RecipeController.java');
 INSERT INTO `messagetable` VALUES ('3', 'tw', '製作處於忙碌狀態，請稍後再試。', '', 'RecipeController.java');
 INSERT INTO `messagetable` VALUES ('3', 'cn', '制作处于忙碌状态，请稍后再试。', '', 'RecipeController.java');
