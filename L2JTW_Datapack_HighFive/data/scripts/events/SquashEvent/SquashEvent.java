b505a0b9d904d18405860853cc484744806e9b9b
pmq <pmq@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2011-09-06 10:57:33 +0000

    High Five sync 8338 (需配合GS-781)
    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@1348 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_HighFive/data/scripts/events/SquashEvent/SquashEvent.java
+++ L2JTW_Datapack_HighFive/data/scripts/events/SquashEvent/SquashEvent.java
@@ -78,6 +78,109 @@ public class SquashEvent extends Quest
 		13016,13017
 	};
 
+/*
+99700	咦？這是什麼？怎麼回事？是誰叫我？
+99701	嘿咻~小葫蘆大爺登場了！
+99702	嘻嘻...你找我嗎...？
+99703	小葫蘆登場了！現在開始會逐漸成長的！
+99704	唉喲，這是多久沒見面的人啊？
+99705	恭喜發財！
+99706	找我嗎？找我幹什麼？以為會出現什麼嗎？
+99707	蹦蹦跳跳！想看我的美貌嗎？
+99708	呃吭！出來了！那就好好伺候我看看吧！
+99709	養得好，恭喜發財~養不好，變成乞丐~
+99710	嘿咻~變~身~！
+99711	你猜我是優良還是不良的呢？
+99712	呀呼，長大了！放馬過來！
+99713	蹦蹦跳跳！長大吧變強吧！
+99714	變成窮光蛋！..可是已經長大了？
+99715	恭喜發財！好好的打破看看！
+99716	都長大了！那就來走人囉~
+99717	若放過我就給你一千萬金幣！…學得像嗎？
+99718	來！你覺得我裡面裝著什麼？
+99719	很好很好，做的很~ 好。那現在知道該做什麼了吧？
+99720	喂！用點心做！都流出來了嘛！
+99721	啊~真舒服！再澆澆看！
+99722	咕嚕咕嚕~很好！還有沒有啊？
+99723	瞄準都不會嗎？流的滿地都是~
+99724	這是不是混了水啊？味道怎會這樣呢？
+99725	喲~很好很好。再繼續，慢慢的好起來了。
+99726	喂！不是那兒！是這裡啦，這裡！是看我小，所以隨便澆澆是嗎？
+99727	嗚嘔呸呸！你這是什麼！是神酒嗎？
+99728	好好做！只要餵五瓶就會變成大葫蘆的！
+99729	神酒中最好的果然是葫蘆神酒！哈哈哈哈！
+99730	唉呀？打我？打我是嗎？
+99731	唉喲！竟然現在給我亂打？不是要你澆神酒嗎？
+99732	喂喂，夠了吧？這樣會枯哦？
+99733	不養我反而想吃掉我是嗎？好，隨你便~不給神酒就死給你看！
+99734	再打呀！再打呀！
+99735	這樣會枯的~我不管囉~
+99736	喂，我這樣死了，什麼寶物都沒有了哦？神酒有那麼珍貴嗎？
+99737	你就好好~的白費工夫吧！
+99738	主人救救我吧~連一滴神酒都沒嚐過就要死啦~
+99739	這樣死了就變成乞丐啦~
+99740	力量漸漸上升哦！再等30秒就可以開溜了！
+99741	20秒後就與你們說再見了！
+99742	好耶，只剩10秒了！ 9... 8... 7...!
+99743	要是不給神酒的話，2分鐘後就會跑掉囉~
+99744	要是不給神酒的話，1分鐘後就說拜拜囉~
+99745	好耶！逃跑成功！這些笨蛋，好自為之吧！
+99746	各位再見~很可惜，大葫蘆我要消失啦！
+99747	為時已晚~我要走人了~好好玩兒吧~
+99748	噹噹噹噹~結束了。收起所有武器，挑戰下個機會吧~
+99749	真可惜耶~都養大了還讓它給跑了！要是我，早就去撞牆啦！哇哈哈哈！
+99750	哦，聲音不錯喔？
+99751	樂器不錯，但沒有歌曲耶。要我來唱一首嗎？
+99752	真棒的音樂！
+99753	感覺真好~再彈彈看！
+99754	克魯諾的旋律真令人傾心！
+99755	好好的配配音！那邊錯了！
+99756	真是愛打混啊！非常好！
+99757	啊啊~~身子就要敞開了！
+99758	哦，這合音！真是美呆了！再彈彈看！
+99759	就是這個！這就是我要的旋律！你，想不想當歌星啊？
+99760	再怎麼敲這玩意兒，也得不到好東西的啦！
+99761	很痛耶！只能用樂器敲啦！
+99762	只有好音樂才能敞開我的身子！
+99763	這個不對，不是有那個嗎？就是拿來當每一章紀念品的，就用那個敲吧！
+99764	為何沒有音樂？我要走囉？真的要走了喔？
+99765	不是這種刺耳的噪音！用美妙的旋律啦！
+99766	這大葫蘆只有用音樂才能打得開~用武器是不行的！
+99767	用樂器敲啦！就跟你說不是這個，用樂器敲啦！
+99768	很有實力哦！但全都是白費工夫啦~
+99769	用那鐵片怎麼不去外頭狩獵怪物呢？我要的是克魯諾！
+99770	葫蘆爆開啦！！！
+99771	你去給我變成乞丐吧！
+99772	爆出些妖怪吧！
+99773	恭喜發財！！
+99774	別把我的死訊流傳於外！
+99775	啊，腸子流出來了！
+99776	拿著這個給我滾蛋~
+99777	喀啊啊！好火大！本來能逃掉的…！
+99778	各位，這裡的葫蘆爆開啦！有寶物掉下來哦！哇哈哈哈
+99779	哎喲！爆開啦！裡面的東西嘩啦嘩啦~
+99780	哇哈哈哈，好好的打打看！
+99781	唉呀？還真有兩把刷子哦？
+99782	再打呀！再打！
+99783	我因你的打擊而成長！
+99784	再使點勁兒~這樣我會走人的喔~
+99785	才這麼點力就以為我會爆開嗎？
+99786	不錯的攻擊，還算能打死隻蒼蠅。
+99787	那裡那裡！右邊一點！啊~好舒服。
+99788	那也能算是攻擊嗎？找個有實力點的來吧！
+99789	不用去想！只管打！打吧！
+99790	把神酒拿來！葫蘆神酒！
+99791	只有喝神酒，我才能長大哦~
+99792	來，快養養看吧！養得好恭喜發財，養不好變成乞丐！
+99793	給我一點神酒吧~肚子好餓~
+99794	來，快去拿神酒來吧。
+99795	若拿神酒來，喝了之後我就為你快速的成長！
+99796	這樣的小葫蘆也要吃？拿神酒來看看，我就再長大給你看！
+99797	哇哈哈哈，養好有好東西~那養不好呢？我也不知道~
+99798	想要發大財嗎？但我喜歡小乞丐耶~
+99799	來，相信我，儘管將神酒澆上！！我會讓你發大財啦！！！
+99601	啦啦啦~今天又是個愉快的旅途~這次去找什麼東西好呢~
+*/
 	private static final String[] SPAWN_TEXT =
 	{
 		"唉喲，這是多久沒見面的人啊？",
@@ -351,7 +454,7 @@ public class SquashEvent extends Quest
 	{
 		if (event == "countdown")
 		{
-			final L2ChronoMonsterInstance mob = (L2ChronoMonsterInstance)npc;
+			final L2ChronoMonsterInstance mob = ((L2ChronoMonsterInstance)npc);
 			final TheInstance self = get(mob);
 			int timeLeft = (int)((self.despawnTime - System.currentTimeMillis()) / 1000);
 			if (timeLeft == 30)
@@ -370,7 +473,7 @@ public class SquashEvent extends Quest
 			else if ((timeLeft % 60) == 0)
 			{
 				if (self.nectar == 0)
-					autoChat(mob, "要是不給神酒的話" + timeLeft/60 + "分鐘後就會跑掉囉~");
+					autoChat(mob, "要是不給神酒的話" + timeLeft / 60 + "分鐘後就會跑掉囉~");
 			}
 		}
 		else if (event == "despawn")
@@ -380,22 +483,19 @@ public class SquashEvent extends Quest
 		}
 		else if (event == "sound")
 		{
-			final L2ChronoMonsterInstance mob = (L2ChronoMonsterInstance)npc;
+			final L2ChronoMonsterInstance mob = ((L2ChronoMonsterInstance)npc);
 			mob.broadcastPacket(new PlaySound(0, "ItemSound3.sys_sow_success", 0, 0, 0, 0, 0));
 		}
-		else
-			return super.onAdvEvent(event, npc, player);
-		return null;
+		return "";
+		//return super.onAdvEvent(event, npc, player);
 	}
 
 	@Override
 	public String onAttack(L2Npc npc, L2PcInstance attacker, int damage, boolean isPet)
 	{
-		final L2ChronoMonsterInstance mob = (L2ChronoMonsterInstance)npc;
+		final L2ChronoMonsterInstance mob = ((L2ChronoMonsterInstance)npc);
 		L2Weapon weapon;
-		final boolean isChronoAttack
-			= !isPet
-			&& (weapon = attacker.getActiveWeaponItem()) != null && contains(CHRONO_LIST, weapon.getItemId());
+		final boolean isChronoAttack = !isPet && (weapon = attacker.getActiveWeaponItem()) != null && contains(CHRONO_LIST, weapon.getItemId());
 		switch (mob.getNpcId())
 		{
 			case 12774:
@@ -441,9 +541,9 @@ public class SquashEvent extends Quest
 	@Override
 	public String onSkillSee(L2Npc npc, L2PcInstance caster, L2Skill skill, L2Object[] targets, boolean isPet)
 	{
-		if (skill.getId() == NECTAR_SKILL && targets[0] == npc)
+		final L2ChronoMonsterInstance mob = ((L2ChronoMonsterInstance)npc);
+		if (skill.getId() == NECTAR_SKILL && targets[0] == mob)
 		{
-			final L2ChronoMonsterInstance mob = (L2ChronoMonsterInstance)npc;
 			switch(mob.getNpcId())
 			{
 				case 12774:
@@ -452,13 +552,13 @@ public class SquashEvent extends Quest
 						if(Rnd.get(100) < 50)
 						{
 							nectarText(mob);
-							npc.doCast(SkillTable.getInstance().getInfo(4514, 1));
+							mob.doCast(SkillTable.getInstance().getInfo(4514, 1));
 							w_nectar++;
 						}
 						else
 						{
 							nectarText(mob);
-							npc.doCast(SkillTable.getInstance().getInfo(4513, 1));
+							mob.doCast(SkillTable.getInstance().getInfo(4513, 1));
 							w_nectar++;
 							_numAtk++;
 						}
@@ -485,13 +585,13 @@ public class SquashEvent extends Quest
 						if(Rnd.get(100) < 50)
 						{
 							nectarText(mob);
-							npc.doCast(SkillTable.getInstance().getInfo(4514, 1));
+							mob.doCast(SkillTable.getInstance().getInfo(4514, 1));
 							w_nectar++;
 						}
 						else
 						{
 							nectarText(mob);
-							npc.doCast(SkillTable.getInstance().getInfo(4513, 1));
+							mob.doCast(SkillTable.getInstance().getInfo(4513, 1));
 							w_nectar++;
 							_numAtk++;
 						}
@@ -513,11 +613,11 @@ public class SquashEvent extends Quest
 					//randomSpawn(12779, 12778, 13017, mob);
 					break;
 				case 12775:
-					npc.doCast(SkillTable.getInstance().getInfo(4513, 1));
+					mob.doCast(SkillTable.getInstance().getInfo(4513, 1));
 					randomSpawn(13016, mob);
 					break;
 				case 12778:
-					npc.doCast(SkillTable.getInstance().getInfo(4513, 1));
+					mob.doCast(SkillTable.getInstance().getInfo(4513, 1));
 					randomSpawn(13017, mob);
 					break;
 				case 12776:
@@ -526,14 +626,13 @@ public class SquashEvent extends Quest
 					break;
 			}
 		}
-		return null;
-		//return super.onSkillSee(npc,caster,skill,targets,isPet);
+		return super.onSkillSee(npc,caster,skill,targets,isPet);
 	}
 
 	@Override
 	public String onKill(L2Npc npc, L2PcInstance killer, boolean isPet)
 	{
-		final L2ChronoMonsterInstance mob = (L2ChronoMonsterInstance)npc;
+		final L2ChronoMonsterInstance mob = ((L2ChronoMonsterInstance)npc);
 		remove(mob);
 		autoChat(mob, KILL_TEXT[Rnd.get(KILL_TEXT.length)]);
 		dropItem(mob, killer);
@@ -546,7 +645,7 @@ public class SquashEvent extends Quest
 	{
 		assert npc instanceof L2ChronoMonsterInstance;
 		
-		final L2ChronoMonsterInstance mob = (L2ChronoMonsterInstance)npc;
+		final L2ChronoMonsterInstance mob = ((L2ChronoMonsterInstance)npc);
 		mob.setOnKillDelay(1500);	//Default 5000ms.
 		final TheInstance self = create(mob);
 		switch(mob.getNpcId())
@@ -575,8 +674,7 @@ public class SquashEvent extends Quest
 			default:
 				throw new RuntimeException();
 		}
-		return null;
-		//return super.onSpawn(npc);
+		return super.onSpawn(npc);
 	}
 
 	static {
