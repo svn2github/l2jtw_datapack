823c446e61fd65c7685c986c9527a2260f6e9005
pmq <pmq@9bd44e3e-6552-0410-907e-a6ca40b856b6>
2011-10-05 03:04:17 +0000

    git-svn-id: http://www.l2jtw.com:3690/svn/datapack@1382 9bd44e3e-6552-0410-907e-a6ca40b856b6

--- L2JTW_Datapack_HighFive/dist/game/data/scripts/handlers/effecthandlers/EffectRecoBonus.java
+++ L2JTW_Datapack_HighFive/dist/game/data/scripts/handlers/effecthandlers/EffectRecoBonus.java
@@ -8,66 +8,59 @@ import com.l2jserver.gameserver.templates.skills.L2EffectType;
 
 
 /**
- * 
- * @author JaJa
- *
+ * @author JaJa (l2.brick)
  */
 
-public class EffectRecoBonus extends L2Effect {
-        
-        public EffectRecoBonus(Env env, EffectTemplate template)
-        {
-                super(env, template);
-        }
-
-        /**
-         * 
-         * @see l2.brick.gameserver.model.L2Effect#getEffectType()
-         */
-        @Override
-        public L2EffectType getEffectType()
-        {
-                return L2EffectType.BUFF;
-        }
-        
-        /**
-         * 
-         * @see l2.brick.gameserver.model.L2Effect#onStart()
-         */
-        @Override
-        public boolean onStart()
-        {
-                if (!(getEffected() instanceof L2PcInstance))
-                        return false;
-                
-                ((L2PcInstance) getEffected()).setRecomBonusType(1).setRecoBonusActive(true);
-                return true;
-        }
-        
-        /**
-         * 
-         * @see l2.brick.gameserver.model.L2Effect#onExit()
-         */
-        @Override
-        public void onExit()
-        {
-                ((L2PcInstance) getEffected()).setRecomBonusType(0).setRecoBonusActive(false);
-        }
-        
-        @Override
-        protected boolean effectCanBeStolen()
-        {
-                return false;
-        }
-        
-        /**
-         * 
-         * @see l2.brick.gameserver.model.L2Effect#onActionTime()
-         */
-        @Override
-        public boolean onActionTime()
-        {
-                return false;
-        }       
-        
+public class EffectRecoBonus extends L2Effect
+{
+	public EffectRecoBonus(Env env, EffectTemplate template)
+	{
+		super(env, template);
+	}
+	
+	/**
+	 * @see com.l2jserver.gameserver.model.L2Effect#getEffectType()
+	 */
+	@Override
+	public L2EffectType getEffectType()
+	{
+		return L2EffectType.BUFF;
+	}
+	
+	/**
+	 * @see com.l2jserver.gameserver.model.L2Effect#onStart()
+	 */
+	@Override
+	public boolean onStart()
+	{
+		if (!(getEffected() instanceof L2PcInstance))
+			return false;
+			
+			((L2PcInstance) getEffected()).setRecomBonusType(1).setRecoBonusActive(true);
+				return true;
+	}
+	
+	/**
+	 * @see com.l2jserver.gameserver.model.L2Effect#onExit()
+	 */
+	@Override
+	public void onExit()
+	{
+		((L2PcInstance) getEffected()).setRecomBonusType(0).setRecoBonusActive(false);
+	}
+	
+	@Override
+	protected boolean effectCanBeStolen()
+	{
+		return false;
+	}
+	
+	/**
+	 * @see com.l2jserver.gameserver.model.L2Effect#onActionTime()
+	 */
+	@Override
+	public boolean onActionTime()
+	{
+		return false;
+	}       
 }
